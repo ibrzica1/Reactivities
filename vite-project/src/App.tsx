1220d55692f596ffddd09ef18d9d490e70479d5e
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-11-21 14:44:12 +0100

    Buggy

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,31 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/activities')
+    .then(response => {
+      console.log('API Response:', response.data);
+      setActivities(response.data)
+    })
+  }, [])
 
   return (
-    <h1>Reactivities</h1>
+    
+    <div>
+       <h1>Reactivities</h1>
+       <ul>
+       {activities.map((activity:any) => (
+        <li key={activity.id}>
+          {activity.Title}
+        </li>
+       ))}
+       </ul>
+    </div>
   )
 }
 
